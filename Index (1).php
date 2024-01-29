7240ae1c8909883a97459108522ad22fc5b15f90
Kishore <94885194+nck79345@users.noreply.github.com>
2024-01-29 12:00:40 +0530

    Add files via upload
    updated

---
+++ Index (1).php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1>Hello, World!</h1>
+        <h2>Welocme to the proplumbers DevOps solutions</h2>
+</body>
+</html>
